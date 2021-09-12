22ec66e7d343159a559bd6fdd40433aca66c0973
RudyKhabib <90527241+RudyKhabib@users.noreply.github.com>
2021-09-13 01:18:19 +0300

    task 5!

--- Habib2.cpp
+++ Habib2.cpp
@@ -2,10 +2,20 @@
 //
 
 #include <iostream>
+#include <string>
+#include<Windows.h>
+
 
 int main()
 {
-    std::cout << "Hello World!\n"<<std::endl;
+   
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string name="";
+    std::cout << "Введите ваше имя"<<std::endl;
+    std::cin >> name;
+    std::cout << "Привет, " << name <<  std::endl;
+
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
