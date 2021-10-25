005eaba21ad58d8eeaea4ffcb30c988b7feb7167
Руди <rudykhabib@mail.ru>
2021-10-25 11:32:41 +0300

    g

--- 17 class complex numbers/17 class complex numbers/stdafx.cpp
+++ 17 class complex numbers/17 class complex numbers/stdafx.cpp
@@ -1,4 +1,4 @@
-// stdafx.cpp : source file that includes just the standard includes
+//3 stdafx.cpp : source file that includes just the standard includes
 // 17 class complex numbers.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
