b3b00d2f883f4e2d20262f1d05fca8cffb56c0af
Руди <rudykhabib@mail.ru>
2021-10-30 16:33:58 +0300

    недоработано

---
+++ 16 sorting/16 sorting/16 sorting.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void swapfunction1(int *a,int *b){
+	int swaper;
+	swaper=*a;
+	*a=*b;
+	*b=swaper;
+}
+
+int main() {
+	int random[100];
+	int N,i=0;
+	cout << "Enter a number from 2 to 100\n";
+	cin >> N;
+	if (N>100 || N<=1) {
+		cout << "Error, enter a number from 2 to 100\n";
+		cin >> N;
+	}
+	for (i=0;i<N;i++) {
+		random[i]=rand();
+		cout<<random[i]<<" ";
+	}
+	cout<<endl;
+	for (int startIndex = 0; startIndex < N - 1;startIndex++)
+	{
+		int smallestIndex = startIndex;
+		for (int currentIndex = startIndex + 1; currentIndex < N;currentIndex++)
+		{
+			if (random[currentIndex] < random[smallestIndex])
+				smallestIndex = currentIndex;
+		}
+		swapfunction1(&random[startIndex],&random[smallestIndex]);
+	}
+	for (i=0;i<N;i++)
+		cout<<random[i]<<" ";
+	cout<<endl;
+	return 0;
+}
