2e5aeb5b2f0916586791f10bcbfeba8ed0e3014b
Руди <rudykhabib@mail.ru>
2021-12-11 10:58:18 +0300

    dds

---
+++ 19 Fifio lifo/SUPERIFO.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "SUPERIFO.h"
+using namespace std;
+ 
+void SUPERIFO :: push(int newelement) {
+	int i;
+	int *container = new int[m_N+1];
+	for(i=0;i<m_N;i++)
+		container[i+1]=m_container[i];
+	container[0]=newelement;
+	delete[] m_container;
+	m_container=container;
+	m_N=m_N+1;
+}
+
+void SUPERIFO :: output(){
+	int i;
+	for(i=0;i<m_N;i++)
+		cout << m_container[i] << " ";
+	cout << endl;
+}
+
+int FIFO :: pop(){
+	int i,temp;
+	if (m_N>1){
+		int *containerl = new int[m_N-1];
+		for(i=0;i<(m_N-1);i++)
+			containerl[i]=m_container[i+1];
+		temp=m_container[0];
+		delete[] m_container;
+		m_container=containerl;
+		m_N=m_N-1;
+		return temp;
+	}
+	else 
+		cout << "The numbers are over" << endl;
+		return m_container[0];
+}
+
+int LIFO :: pop(){
+	int i,temp;
+	if (m_N>1){
+		int *containerl = new int[m_N-1];
+		for(i=0;i<(m_N-1);i++)
+			containerl[i]=m_container[i];
+		temp=m_container[m_N-1];
+		delete[] m_container;
+		m_container=containerl;
+		m_N=m_N-1;
+		return temp;
+	}
+	else 
+		cout << "The numbers are over" << endl;
+		return m_container[0];
+}
